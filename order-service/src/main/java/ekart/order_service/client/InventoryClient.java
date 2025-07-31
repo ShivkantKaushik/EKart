de6819910e25f34256099b70d37ebf44e99a7019
shivkantkaushik <shivkantkaushik31@gmail.com>
2025-07-31 16:52:31 +0530

    migrated from feign client to http client interfaces by spring boot 3

--- order-service/src/main/java/ekart/order_service/client/InventoryClient.java
+++ order-service/src/main/java/ekart/order_service/client/InventoryClient.java
@@ -4,14 +4,21 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
 //@FeignClient(value = "inventory", url = "http://localhost:4002") // with hardcoding inventory service url
 
-@FeignClient(value = "inventory", url = "${inventory.service.url}") //url from application.properties
+//@FeignClient(value = "inventory", url = "${inventory.service.url}") //url from application.properties
+
+//removing feign clients, from spring version 3, we have http interface clients, where
+//we have similar functionality inbuilt
+
 public interface InventoryClient {
 
 
-    @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
+//    @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
+
+    @GetExchange("/api/inventory")
     boolean inStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 
 }

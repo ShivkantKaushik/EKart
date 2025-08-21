f83fdc201462619ddffa045a777ce3bd1cfa0690
shivkantkaushik <shivkantkaushik31@gmail.com>
2025-08-21 23:29:53 +0530

    integrated kafka in order service, added code for orderPlacedEvent

---
+++ order-service/src/main/java/ekart/order_service/event/OrderPlacedEvent.java
@@ -0,0 +1,29 @@
+package ekart.order_service.event;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderPlacedEvent {
+
+    private String orderNumber;
+    private String email;
+
+
+    public String getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(String orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- order-service/src/main/java/ekart/order_service/service/OrderService.java
+++ order-service/src/main/java/ekart/order_service/service/OrderService.java
@@ -2,9 +2,13 @@ package ekart.order_service.service;
 
 import ekart.order_service.client.InventoryClient;
 import ekart.order_service.dto.OrderRequest;
+import ekart.order_service.event.OrderPlacedEvent;
 import ekart.order_service.model.Order;
 import ekart.order_service.repository.OrderRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -13,11 +17,16 @@ import java.util.UUID;
 @Service
 public class OrderService {
 
+    private static final Logger log = LoggerFactory.getLogger(OrderService.class);
     @Autowired
     private OrderRepository orderRepository;
 
     @Autowired
     private InventoryClient inventoryClient;
+
+
+    @Autowired
+    private KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) throws Exception {
 
@@ -31,6 +40,14 @@ public class OrderService {
            order.setSkuCode(orderRequest.skuCode());
            order.setQuantity(orderRequest.quantity());
            orderRepository.save(order);
+
+           //send message to kafka topic
+           OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(), orderRequest.userDetails().email());
+           log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
+           kafkaTemplate.send("order-placed", orderPlacedEvent);
+           log.info("End - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
+
+
        }else {
            throw new Exception("Product with SKU Code " + orderRequest.skuCode() + " not in stock.");
        }

8c26c1eeba2c772901bd9791aaa752101f1ba2cf
shivkantkaushik <shivkantkaushik31@gmail.com>
2025-08-23 22:33:33 +0530

    order service was not running, there was error in RestClientConfig in getClientRequestFactory, had to add http client 5 dependency

--- order-service/src/main/java/ekart/order_service/config/RestClientConfig.java
+++ order-service/src/main/java/ekart/order_service/config/RestClientConfig.java
@@ -2,6 +2,8 @@ package ekart.order_service.config;
 
 import ekart.order_service.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.http.client.ClientHttpRequestFactorySettings;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
@@ -9,6 +11,8 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -32,6 +36,7 @@ public class RestClientConfig {
 //    connectTimeout: How long to wait to connect to the server.
 //    readTimeout: How long to wait for the server to respond after connection.
 
+    //for this add http client 5 dependency, without that there was error
     private ClientHttpRequestFactory getClientRequestFactory() {
         HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
         factory.setConnectTimeout(3000);
